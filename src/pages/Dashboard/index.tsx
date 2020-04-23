1fd461a04cdeb54322189b801d23a5c2b820732f
Dtesch9 <alucard.s8123@gmail.com>
2020-04-23 12:40:07 -0300

    All tests Succeeded

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,138 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { parseISO, format } from 'date-fns';
+import { pt } from 'date-fns/locale';
+
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+
+import api from '../../services/api';
+
+import Header from '../../components/Header';
+
+import formatValue from '../../utils/formatValue';
+
+import { Container, CardContainer, Card, TableContainer } from './styles';
+
+interface Transaction {
+  id: string;
+  title: string;
+  value: number;
+  formattedValue: string;
+  formattedDate: string;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: string;
+}
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface AxiosReponse {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
+const Dashboard: React.FC = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
+
+  const formatTransactions = useCallback(
+    (data: Transaction[]): Transaction[] => {
+      return data.map(transaction => {
+        const formattedValue = formatValue(transaction.value);
+        const formattedDate = format(
+          parseISO(transaction.created_at),
+          'dd/MM/yyyy',
+          {
+            locale: pt,
+          },
+        );
+
+        return { ...transaction, formattedValue, formattedDate };
+      });
+    },
+    [],
+  );
+
+  useEffect(() => {
+    async function loadTransactions(): Promise<void> {
+      const response = await api.get<AxiosReponse>('transactions');
+
+      const formattedTransactions = formatTransactions(
+        response.data.transactions,
+      );
+
+      setTransactions(formattedTransactions);
+      setBalance(response.data.balance);
+    }
+
+    loadTransactions();
+  }, [formatTransactions]);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <CardContainer>
+          <Card>
+            <header>
+              <p>Entradas</p>
+              <img src={income} alt="Income" />
+            </header>
+            <h1 data-testid="balance-income">{formatValue(balance.income)}</h1>
+          </Card>
+          <Card>
+            <header>
+              <p>Saídas</p>
+              <img src={outcome} alt="Outcome" />
+            </header>
+            <h1 data-testid="balance-outcome">
+              {formatValue(balance.outcome)}
+            </h1>
+          </Card>
+          <Card total>
+            <header>
+              <p>Total</p>
+              <img src={total} alt="Total" />
+            </header>
+            <h1 data-testid="balance-total">{formatValue(balance.total)}</h1>
+          </Card>
+        </CardContainer>
+
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'outcome'
+                      ? `- ${transaction.formattedValue}`
+                      : transaction.formattedValue}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.formattedDate}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TableContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
